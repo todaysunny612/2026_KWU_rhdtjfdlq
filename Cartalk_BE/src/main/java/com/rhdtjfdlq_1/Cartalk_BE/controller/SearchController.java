05d8a38f7726d74583d29b8f7109f0401bfe2927
최희선 <todaysunny612@gmail.com>
2026-04-07 18:38:55 +0900

    [feature] 차량 번호 조회 서비스 구현 및 차량 번호 등록/조회시 정규화 기능 추가

---
+++ Cartalk_BE/src/main/java/com/rhdtjfdlq_1/Cartalk_BE/controller/SearchController.java
@@ -0,0 +1,4 @@
+package com.rhdtjfdlq_1.Cartalk_BE.controller;
+
+public class SearchController {
+}
---
+++ Cartalk_BE/src/main/java/com/rhdtjfdlq_1/Cartalk_BE/service/impl/SearchServiceImpl.java
@@ -0,0 +1,4 @@
+package com.rhdtjfdlq_1.Cartalk_BE.service.impl;
+
+public class SearchServiceImpl {
+}
